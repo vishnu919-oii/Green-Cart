ef3f8d8f45defa6be2f1ebdae916f519d509752e
Vishnu <vishnuanish919@gmail.com>
2025-11-06 22:39:05 +0530

    Recommit after removing sensitive .env file

--- client/src/pages/MyOrders.jsx
+++ client/src/pages/MyOrders.jsx
@@ -57,7 +57,7 @@ const MyOrders = () => {
               </div>
 
               <div className="flex flex-col justify-center md:ml-8 mb-4 md:mb-0">
-                <p>Quantity: {item.qunatity || "1"}</p>
+                <p>Quantity: {item.quantity || "1"}</p>
                 <p>Status: {order.status}</p>
                 <p>Date: {new Date(order.createdAt).toLocaleDateString()}</p>
                 </div>
