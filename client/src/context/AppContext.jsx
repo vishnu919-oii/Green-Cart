b3b16a6dd5f8376ebb5480af05961f79b827fa93
Vishnu <vishnuanish919@gmail.com>
2025-11-09 22:23:28 +0530

    after finished

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -6,7 +6,7 @@ import toast from "react-hot-toast";
 import axios from "axios";
 
 axios.defaults.withCredentials = true;
-axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:4000' ||'https://green-cart-backend-gamma.vercel.app/';
+axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:4000' ||'https://green-cart-dun-three.vercel.app/';
 
 export const AppContext = createContext();
 
