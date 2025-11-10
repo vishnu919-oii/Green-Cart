b8b93130bf822d042fc874536cc2d1752de742c8
Vishnu <vishnuanish919@gmail.com>
2025-11-10 17:03:17 +0530

    after finished

--- server/server.js
+++ server/server.js
@@ -24,11 +24,11 @@ const allowedOrigins = [
   "http://localhost:5173",
   "https://green-cart-frontend-zeta.vercel.app"
 ];
-app.post('/stripe', express.raw({ type: 'application/json' }), stripeWebhooks);
 app.use(cors({
   origin: allowedOrigins,
   credentials: true,
 }));
+app.post('/stripe', express.raw({ type: 'application/json' }), stripeWebhooks);
 
 
 
