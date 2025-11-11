50b0b9bb1c0a23ac49314a48ce49f6ad5bba0ca0
Vishnu <vishnuanish919@gmail.com>
2025-11-11 18:54:44 +0530

    after finished

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -69,8 +69,8 @@ export const login = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      secure: true,
+      sameSite:"none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
