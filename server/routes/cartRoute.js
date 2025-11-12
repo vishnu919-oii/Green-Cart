6902cae963ff4f354e4856e17162629436b9b948
Vishnu <vishnuanish919@gmail.com>
2025-11-12 11:04:39 +0530

    after finished

--- server/routes/cartRoute.js
+++ server/routes/cartRoute.js
@@ -1,9 +1,11 @@
 import express from "express";
 import authUser from "../middlewares/authUser.js";
-import { updateCart } from "../controllers/cartController.js";
+import { addToCart, updateCart } from "../controllers/cartController.js";
 
 const cartRouter = express.Router();
 
+
+cartRouter.post("/add", authUser, addToCart);
 cartRouter.post("/update", authUser, updateCart);
 
 export default cartRouter;
